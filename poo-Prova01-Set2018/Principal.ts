ae7602198182fa47964885626a5121cc94bbe5f3
Juliana Barreto <julichan.sousa@gmail.com>
2018-09-24 16:15:11 -0300

    Atividades-2018.2

--- poo-Prova01-Set2018/Principal.ts
+++ poo-Prova01-Set2018/Principal.ts
@@ -3,4 +3,25 @@ var readline = require('readline-sync');
 import {Secao} from "./Secao";
 import {Artefato} from "./Artefato";
 
-let codigoSecao : number = Number(readline)
+let codigoSecao : number = Number(readline.question());
+let anoSecao : number = Number(readline.question());
+let descricaoSecao : string = readline.question();
+let s : Secao = new Secao(codigoSecao,descricaoSecao,anoSecao,[]);
+    for(let i = 0; i < 3; i ++){
+        let id : number = Number(readline.question());
+        let nome : string = readline.question();
+        let preco : number = Number(readline.question());
+        let a : Artefato = new Artefato(id, nome, "23-2-1004","Quixadá",preco);
+        s.inserir(a);
+    }
+        let identificador : number = Number(readline.question());
+        let a : Artefato | undefined = s.buscar(identificador);
+        if(a == undefined){
+            console.log("o artefato nao esta");
+        }else{
+            a.setPreco(200);
+            a.setPreco(100);
+            console.log(a.getPreco());
+        }
+
+    
--- poo-Prova01-Set2018/Secao.ts
+++ poo-Prova01-Set2018/Secao.ts
@@ -18,6 +18,30 @@ export class Secao{
     public setCodigo(codigo : number) : void{
         this.codigo = codigo;
     }
-    public getArtefatos()
+    public getArtefatos() : Array<Artefato>{
+        return this.artefatos;
+    }
+    public setArtefatos(artefatos : Array<Artefato>) : void{
+        this.artefatos = artefatos;
+    }
+    public buscar(id : number) : Artefato | undefined{
+        for(let a of this.artefatos ){
+            if(a.getId() == id){
+                return a;
+            }
+        }
+        return undefined;
+    }
+    public inserir(a : Artefato) : void{
+        this.artefatos.push(a);
+    }
+    public toString() : string{
+        let res : string = "";
+        res += "Codigo: " + this.codigo + '\n';
+        res += "Descrição: " + this.descricao + '\n';
+        res += "Ano: " + this.ano + '\n';
+        res += "Artefato: " + this.artefatos + '\n';
+        return res;
+    }
 
 }
